13aa7458bb56762f639350b31cd8aa189fb995aa
ViniHipolito <97119997+ViniHipolito@users.noreply.github.com>
2022-09-26 14:29:14 -0300

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,25 @@
+const accessCode1 = "TECLADO";
+const accessCode2 = "RADIO";
+const accessCode3 = "MINECRAFT";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("L E C O T A D", 100,50)
+    fill("lightblue")
+    text("Dica: A Origem da Digitação!", 100,70)
+
+    fill("white")
+    textSize(15)
+    text("I A D R O", 700,150)
+    fill("lightblue")
+    text("Dica: Não é a palavra Irado, Mas era impressionante na Época.", 570,170)
+
+    fill("white")
+    textSize(15)
+    text("F R I T M C A E N", 100,250)
+    fill("lightblue")
+    text("Dica: Jogo sobre Cubos, Lançado em 2011!", 100,270)
+
+}
